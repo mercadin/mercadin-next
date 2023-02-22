44a6b85deece6a0545bb7fe59b9cb571d67ed5b7
vinihcrosa <vinihcrosa@users.noreply.github.com>
2023-02-22 04:56:49 -0300

    log: add signIn logs

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -12,16 +12,19 @@ export default async function SignIn(
   res: NextApiResponse
 ) {
   if( req.method !== "POST" ) {
+    console.log("Method not allowed")
     return res.status(400).json({ error: "Method not allowed" });
   }
 
   const token = req.cookies.token;
   if(token) {
+    console.log("Already logged in")
     return res.status(400).json({ error: "Already logged in" });
   }
 
   const { email, password } = req.body;
   if( !email || !password ) {
+    console.log("Please add all the fields");
     return res.status(400).json({ error: "Please add all the fields" });
   }
   const user = await User.findOne({
@@ -29,7 +32,7 @@ export default async function SignIn(
   })
 
   if( !user ) {
-    console.log(user)
+    console.log("User not exists")
     return res.status(400).json({ error: "User not exists" });
   }
 
@@ -37,6 +40,7 @@ export default async function SignIn(
     const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET as string);
     res.setHeader("Set-Cookie", `user=${user._id}; path=/, httponly`);
     res.setHeader("Set-Cookie", `token=${token}; path=/`);
+    console.log("Logged in", user.email)
     return res.status(200).json({ user: UserView(user), token });
   }
 }
